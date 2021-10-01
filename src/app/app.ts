cbebc766e2872dee404c2b6f6674fc36adccc3a1
misupopo <kokorononakaniitumo@yahoo.co.jp>
2021-10-01 17:38:49 +0900

    add local dynamodb docker

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,26 @@
 let response;
 import * as log from 'lambda-log';
 import { UserService } from './service';
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: 'ap-northeast-1',
+  // endpoint: 'http://dynamodb:8000'
+});
+
+const makeClient = () => {
+  // const dynamoDbClient = new AWS.DynamoDB({
+  //   endpoint: 'http://localhost:8000',
+  //   // accessKeyId: awsAccessKeyId,
+  //   // secretAccessKey: awsAccessKey,
+  //   region: 'ap-northeast-1'
+  // })
+  const dynamoDB = new AWS.DynamoDB({
+    endpoint: 'http://docker.for.mac.localhost:8000',
+    maxRetries: 10
+  });
+  return dynamoDB
+}
 
 /**
  *
@@ -22,10 +42,13 @@ exports.lambdaHandler = async (event: AWSLambda.APIGatewayEvent, context: AWSLam
     // context,
     stage: process.env.NODE_ENV,
   }
-  log.info('Start process');
-  log.info('Start process');
 
   try {
+    const dbClient = makeClient();
+
+    log.info('debug1111');
+    log.info(await dbClient.listTables({}).promise());
+
     const userService = new UserService();
 
     // const ret = await axios(url);
@@ -37,8 +60,13 @@ exports.lambdaHandler = async (event: AWSLambda.APIGatewayEvent, context: AWSLam
       }),
     };
   } catch (err) {
-    console.log(err);
-    return err;
+    log.error(err);
+    return {
+      'statusCode': 500,
+      'body': JSON.stringify({
+        message: `${err.message}`
+      }),
+    };
   }
 
   return response;
