edc2529c8a3fcade350dd910a9b82e3e22c3a22e
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-11-10 18:16:15 +0900

    modify docker compose yaml

--- 1_Create_SQS_SNS.sh
+++ 1_Create_SQS_SNS.sh
@@ -2,4 +2,10 @@
 
 awslocal sqs create-queue --queue-name sqs_queue_scrape
 awslocal sqs create-queue --queue-name sqs_queue_notify
+awslocal sns create-topic --name sns_topic_price
 
+mobile_num="+901234567890"
+awslocal sns subscribe \
+    --topic-arn arn:aws:sns:ap-northeast-1:000000000000:sns_topic_price \
+    --protocol sms \
+    --notification-endpoint $mobile_num
